89dc9a81178383d83be79014c876584b09d96249
mecolela <sichangim@gmail.com>
2017-10-06 14:02:29 +0300

    geo polygon points added

---
+++ config/index.js
@@ -0,0 +1,136 @@
+"use strict";
+
+module.exports = {
+    mongoConfig: {
+        url: 'mongodb://heroku_wbndnvpk:unipoolpass1@ds159254.mlab.com:59254/heroku_wbndnvpk'
+    },
+    mqttConfig: {
+        port: 80
+    },
+    geoPolyArray: [
+        {
+            area: 'South C',
+            path: [
+                {"latitude": -1.30604113237624, "longitude": 36.82649803056847},
+                {"latitude": -1.3068992161363595, "longitude": 36.82100486650597},
+                {"latitude": -1.3110180141005436, "longitude": 36.818429945851676},
+                {"latitude": -1.3142787243509373, "longitude": 36.81963157549034},
+                {"latitude": -1.3203453512903152, "longitude": 36.821022033691406},
+                {"latitude": -1.3249789735443878, "longitude": 36.819047927856445},
+                {"latitude": -1.3295696833415442, "longitude": 36.824026107788086},
+                {"latitude": -1.3298700098534995, "longitude": 36.828274726867676},
+                {"latitude": -1.3270383584325833, "longitude": 36.84316635131836},
+                {"latitude": -1.3250647812822585, "longitude": 36.84629917144775},
+                {"latitude": -1.3190153284910642, "longitude": 36.83677196502685},
+                {"latitude": -1.315823269826343, "longitude": 36.83130454912315},
+                {"latitude": -1.3122193288979696, "longitude": 36.829931258107536}
+
+            ]
+        },
+        {
+            area: 'Kilimani',
+            path: [
+                {"latitude": -1.3003091263999844, "longitude": 36.774330139160156},
+                {"latitude": -1.2901836930023267, "longitude": 36.774587631225586},
+                {"latitude": -1.289660257958964, "longitude": 36.77835559792584},
+                {"latitude": -1.288201505451073, "longitude": 36.780758857203175},
+                {"latitude": -1.286665524490339, "longitude": 36.78196907043457},
+                {"latitude": -1.2852067702689305, "longitude": 36.78351402282715},
+                {"latitude": -1.2834905877652765, "longitude": 36.785573959350586},
+                {"latitude": -1.2852925793638545, "longitude": 36.78763389587402},
+                {"latitude": -1.290432538359997, "longitude": 36.78659534401959},
+                {"latitude": -1.294036510462132, "longitude": 36.78711032815045},
+                {"latitude": -1.2993566503925364, "longitude": 36.78796863503521}
+            ]
+        },
+        {
+            area: 'Langate',
+            path: [{"latitude": -1.325442334769867, "longitude": 36.79371929116314},
+                {"latitude": -1.3267294502848648, "longitude": 36.791659354639705},
+                {"latitude": -1.327593962889348, "longitude": 36.79044270502345},
+                {
+                    "latitude": -1.3283018758896645, "longitude": 36.78896212564723
+                }, {"latitude": -1.3292028557785722, "longitude": 36.78754591928737}, {
+                    "latitude": -1.3299965758845287,
+                    "longitude": 36.786151170599624
+                }, {"latitude": -1.3308096023001634, "longitude": 36.7841963765386}, {
+                    "latitude": -1.3316676775203409,
+                    "longitude": 36.78282308552298
+                }, {"latitude": -1.3326587542897328, "longitude": 36.78141117095947}, {
+                    "latitude": -1.3314960625001953,
+                    "longitude": 36.78093481037649
+                }, {"latitude": -1.33025185324694, "longitude": 36.78024816486868}, {
+                    "latitude": -1.3295267795511303,
+                    "longitude": 36.77355766296387
+                }, {"latitude": -1.3278535311439326, "longitude": 36.772398948669434}, {
+                    "latitude": -1.325794146934162,
+                    "longitude": 36.771111488342285
+                }, {"latitude": -1.3225334517662133, "longitude": 36.76982402801514}, {
+                    "latitude": -1.3225763556778443,
+                    "longitude": 36.77196979522705
+                }, {"latitude": -1.3178140169645716, "longitude": 36.77231311798095}, {
+                    "latitude": -1.318496190104948,
+                    "longitude": 36.77994775745901
+                }, {"latitude": -1.318496190104948, "longitude": 36.782651424146025}, {
+                    "latitude": -1.3181100542398327,
+                    "longitude": 36.784110545850126
+                }, {"latitude": -1.317809726303358, "longitude": 36.786599635815946}, {
+                    "latitude": -1.318581998066844,
+                    "longitude": 36.78801584217581
+                }, {"latitude": -1.319315656282086, "longitude": 36.78909301757812}, {
+                    "latitude": -1.3208130040371955,
+                    "longitude": 36.7902474400762
+                }, {"latitude": -1.3230869119073752, "longitude": 36.79179239246878}]
+        },
+        {
+            area: 'South B',
+            path: [
+                {"latitude": -1.305620671750544, "longitude": 36.827364921045955}, {
+                    "latitude": -1.3050200128436629,
+                    "longitude": 36.82908153481549
+                }, {"latitude": -1.3035612691870009, "longitude": 36.83182811684674}, {
+                    "latitude": -1.3033038437480697,
+                    "longitude": 36.83354473061626
+                }, {"latitude": -1.3035612691870009, "longitude": 36.835604667139705}, {
+                    "latitude": -1.305620671750544,
+                    "longitude": 36.838093757105526
+                }, {"latitude": -1.3070794142124877, "longitude": 36.84006786294049}, {
+                    "latitude": -1.3087955807297835,
+                    "longitude": 36.84290027566021
+                }, {"latitude": -1.31162725291504, "longitude": 36.846590995264705}, {
+                    "latitude": -1.314887962370791,
+                    "longitude": 36.84976673073834
+                }, {"latitude": -1.3153170027717782, "longitude": 36.851826667261776}, {
+                    "latitude": -1.317376395669323,
+                    "longitude": 36.85345745034283
+                }, {"latitude": -1.319435786864622, "longitude": 36.84968090004986}, {
+                    "latitude": -1.3211519448917959,
+                    "longitude": 36.84839343972271
+                }, {"latitude": -1.3220958313015345, "longitude": 36.846590995264705}, {
+                    "latitude": -1.3226106782828742,
+                    "longitude": 36.84453105874127
+                }, {"latitude": -1.32072290549608, "longitude": 36.84118366189068}, {
+                    "latitude": -1.318663515366025,
+                    "longitude": 36.83800792641705
+                }, {"latitude": -1.3163466994331579, "longitude": 36.83500385232037}, {
+                    "latitude": -1.3139440732290448,
+                    "longitude": 36.83251476235455
+                }, {"latitude": -1.3129143755767696, "longitude": 36.830626487208065}, {
+                    "latitude": -1.3090530056061127,
+                    "longitude": 36.82882404275006
+                }]
+        }
+    ],
+    firebaseConfig: {
+        "type": "service_account",
+        "project_id": "uni-pool",
+        "private_key_id": "6a63519865cd570303db84a9294f84869116ae72",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC8v0B/RVEM7GLO\nFN/iq1doLNJugXB9+Qzy5yU7xkmBN2gj9yyRpHy1KFV5r7FdyqqAtKaBBKB1zICW\ncUQuMDeLgOLGHdBY6aUJ1DN4dWWmvtw2x3g8ZY8mbK9PTLUlCILx/zwtEk4y0AaU\n8um48mBJgDTrPENRo191vPE2u80aupbkLxw6wlnrAXRpuQOgyx5xsbHmhc5+v6Tm\nkqLD/tBty4sBjqCTsBf1Zh5avXG5XsrLkQbJEVP+yWpLTrwHP3T8phconFvtGCyu\nu0xk3iKTiFqPJOXWWo/tW5Etmoq4xQ1s8l3KQ8OyCSBG9W7YHX/mesWXiDXmqWOj\nIqMcOXa9AgMBAAECggEAJk8kImDZj0ewUzcp7OoNetu0F1FG2Y6x2eBf7oaztxod\nlUUvIt1xoVxPIvxtwcVnf2mLLP8ZKWepRr+1ELHhKDuv63m0JsO/Z2QOUVn0mJg+\n/pBUX1/Wovwmxe/iCWL1nvQIzPYc8JPH7jXyicMKF4kB1Kf92uVPtAg2Jj+OZOjW\nOpZnBPr3a+Z0ev2417uC1uQv995FlqDte3ukO2J/l8WmMTd9bLvYA/5BxcAO2eIO\n/uSOp01cok9MtVR5svTuRO7fPoYo7xzNnv/OFs4SJop4OpSXDx/CO3sp/1ZXrZA5\n7wHe/bF87oStLpeIP1WApWzU+R+x78NRQEcD31hSyQKBgQDu4kZPw3X3uYYBZjlA\nGariKSTW8C7cRmoHIJu9J9wwlKPJiTbmBxmRBVRGs2DzYoSqDyyr2bXdBbeu0beP\nKNeMOd9fZv/ST2V6YtDQ+wbNKWPjKNMB4XdxW0kKs0hEiEc7GzvAG8o7MDZ557K5\nu1x3JmPXArvXVkZnUFIdW71UuQKBgQDKRVfn/ohXHELziqNAQ6EwbNjNFcq7t+11\nANRaDM3aU1m6Q+Pgjg4cDQir7D6ZLanWlWZBPDbYry1W+0wlUizcAinmvdiTMe/6\nw8nb7sld9R9FDCXuTAZsY+xmdACWAyaWD8MxZlowe4SRElsb8a6yxabnxOd8OiH2\n0n6Ii5H4JQKBgQDGoNTv68mCMvvu2+3Mk8sYBDgClHGSbOYqiP2WGbenmWBa6kIA\noHSste6uXubFWOyQbs+pEPx8DA4ASZbEbaRUj6ZhMEaUu7nYOqQAsEmme7z3m/Ff\nxfbIBaxFPm9ftKTty6YyBalw75TMZlCgyN6IoB4yIK7JzY36x7mPecGC2QKBgEEG\ny/z0UkaW+lsrTdJiK5bS11wPTm7M8ZT04E/W3/L5atY77y2I3bZc5lXHDFTYXkpz\nGz2LqyI8JvpYraEt6ugL9gaNL+waWMkoWmPPP4NqQ1RCVItoe4QrF5vUXT/c4XHK\nvXjLK+F8RCETYqD4MK6eZksKfVTtp2+fOjiRjirVAoGASZBm2c4voIgScg/eP3bu\nSw+OlQHEolmh74ePE03YykKWxNnJjWA3eoXOTaw3W0Fw47udNV2wxZu5Je1flnoN\nbH/wxyEJxqqVsoWpXGBS7FcWFEvNfXGJNuJ86WIrVnWQWLslN/zs/aYvR42sM8JR\nv9knrwjeC/9zWuayMnCeCMk=\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-ghh93@uni-pool.iam.gserviceaccount.com",
+        "client_id": "109907487313957185694",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ghh93%40uni-pool.iam.gserviceaccount.com"
+    }
+};
