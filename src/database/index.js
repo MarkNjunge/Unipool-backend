a1520e3d2a1b17565c22a6037b8799289c416352
mecolela <sichangim@gmail.com>
2017-10-11 17:42:40 +0300

    change to localmongoUrl for testing

--- src/database/index.js
+++ src/database/index.js
@@ -1,37 +1,39 @@
-'use strict';
+'use strict'
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const connectOptions = {
     promiseLibrary: global.Promise,
     useMongoClient: true
-};
-const {log} = require('../middleware/logger');
+}
+const {log} = require('../middleware/logger')
 
 const {
     mongoConfig
-} = require('../config');
+} = require('../config')
 
 function init() {
     return new Promise((resolve, reject) => {
-        const mongoUrl = mongoConfig.url;
-        mongoose.connect(mongoUrl, connectOptions);
-        const db = mongoose.connection;
+        const mongoUrl = mongoConfig.localUrl
+        // const mongoUrl = mongoConfig.url
+        mongoose.connect(mongoUrl, connectOptions)
+        const db = mongoose.connection
 
         db.on('error', (reason) => {
-            reject(reason);
-        });
+            reject(reason)
+        })
 
         db.on('open', () => {
-            log('Mongo Connected');
-            resolve();
-        });
+            log('Mongo Connected')
+            resolve()
+        })
 
         db.on('disconnected', () => {
-            log('Disconnected from mongo', true);
+            log('Disconnected from mongo', true)
         })
     })
 }
 
 module.exports = {
-    init
-};
+    init,
+
+}
