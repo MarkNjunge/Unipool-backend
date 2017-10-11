2ca89e8f3868c626e565b066a603ed2a564c21ec
mecolela <sichangim@gmail.com>
2017-10-11 17:42:12 +0300

    model folder exported

---
+++ src/database/models/index.js
@@ -0,0 +1,11 @@
+const Ride = require('./ride.js')
+const User = require('./user.js')
+const Geolocation = require('./geolocation.js')
+const Vehicle = require('./vehicle.js')
+
+module.exports = {
+    Ride,
+    User,
+    Vehicle,
+    Geolocation
+}
