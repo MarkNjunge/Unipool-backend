932719e695b0f9c40b7e43fedd3873d5e92a48b6
mecolela <sichangim@gmail.com>
2017-10-11 17:40:29 +0300

    local mongo url added

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,23 @@
-"use strict";
+'use strict'
 
 module.exports = {
     mongoConfig: {
-        url: 'mongodb://devTeam:devteampass@ds159254.mlab.com:59254/heroku_wbndnvpk'
+        url: 'mongodb://devTeam:devteampass@ds159254.mlab.com:59254/heroku_wbndnvpk',
+        localUrl: 'mongodb://localhost:27017'
     },
     mqttConfig: {
         port: 80
     },
     firebaseConfig: {
-        "type": "service_account",
-        "project_id": "uni-pool",
-        "private_key_id": "6a63519865cd570303db84a9294f84869116ae72",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC8v0B/RVEM7GLO\nFN/iq1doLNJugXB9+Qzy5yU7xkmBN2gj9yyRpHy1KFV5r7FdyqqAtKaBBKB1zICW\ncUQuMDeLgOLGHdBY6aUJ1DN4dWWmvtw2x3g8ZY8mbK9PTLUlCILx/zwtEk4y0AaU\n8um48mBJgDTrPENRo191vPE2u80aupbkLxw6wlnrAXRpuQOgyx5xsbHmhc5+v6Tm\nkqLD/tBty4sBjqCTsBf1Zh5avXG5XsrLkQbJEVP+yWpLTrwHP3T8phconFvtGCyu\nu0xk3iKTiFqPJOXWWo/tW5Etmoq4xQ1s8l3KQ8OyCSBG9W7YHX/mesWXiDXmqWOj\nIqMcOXa9AgMBAAECggEAJk8kImDZj0ewUzcp7OoNetu0F1FG2Y6x2eBf7oaztxod\nlUUvIt1xoVxPIvxtwcVnf2mLLP8ZKWepRr+1ELHhKDuv63m0JsO/Z2QOUVn0mJg+\n/pBUX1/Wovwmxe/iCWL1nvQIzPYc8JPH7jXyicMKF4kB1Kf92uVPtAg2Jj+OZOjW\nOpZnBPr3a+Z0ev2417uC1uQv995FlqDte3ukO2J/l8WmMTd9bLvYA/5BxcAO2eIO\n/uSOp01cok9MtVR5svTuRO7fPoYo7xzNnv/OFs4SJop4OpSXDx/CO3sp/1ZXrZA5\n7wHe/bF87oStLpeIP1WApWzU+R+x78NRQEcD31hSyQKBgQDu4kZPw3X3uYYBZjlA\nGariKSTW8C7cRmoHIJu9J9wwlKPJiTbmBxmRBVRGs2DzYoSqDyyr2bXdBbeu0beP\nKNeMOd9fZv/ST2V6YtDQ+wbNKWPjKNMB4XdxW0kKs0hEiEc7GzvAG8o7MDZ557K5\nu1x3JmPXArvXVkZnUFIdW71UuQKBgQDKRVfn/ohXHELziqNAQ6EwbNjNFcq7t+11\nANRaDM3aU1m6Q+Pgjg4cDQir7D6ZLanWlWZBPDbYry1W+0wlUizcAinmvdiTMe/6\nw8nb7sld9R9FDCXuTAZsY+xmdACWAyaWD8MxZlowe4SRElsb8a6yxabnxOd8OiH2\n0n6Ii5H4JQKBgQDGoNTv68mCMvvu2+3Mk8sYBDgClHGSbOYqiP2WGbenmWBa6kIA\noHSste6uXubFWOyQbs+pEPx8DA4ASZbEbaRUj6ZhMEaUu7nYOqQAsEmme7z3m/Ff\nxfbIBaxFPm9ftKTty6YyBalw75TMZlCgyN6IoB4yIK7JzY36x7mPecGC2QKBgEEG\ny/z0UkaW+lsrTdJiK5bS11wPTm7M8ZT04E/W3/L5atY77y2I3bZc5lXHDFTYXkpz\nGz2LqyI8JvpYraEt6ugL9gaNL+waWMkoWmPPP4NqQ1RCVItoe4QrF5vUXT/c4XHK\nvXjLK+F8RCETYqD4MK6eZksKfVTtp2+fOjiRjirVAoGASZBm2c4voIgScg/eP3bu\nSw+OlQHEolmh74ePE03YykKWxNnJjWA3eoXOTaw3W0Fw47udNV2wxZu5Je1flnoN\nbH/wxyEJxqqVsoWpXGBS7FcWFEvNfXGJNuJ86WIrVnWQWLslN/zs/aYvR42sM8JR\nv9knrwjeC/9zWuayMnCeCMk=\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-ghh93@uni-pool.iam.gserviceaccount.com",
-        "client_id": "109907487313957185694",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://accounts.google.com/o/oauth2/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ghh93%40uni-pool.iam.gserviceaccount.com"
+        'type': 'service_account',
+        'project_id': 'uni-pool',
+        'private_key_id': '6a63519865cd570303db84a9294f84869116ae72',
+        'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC8v0B/RVEM7GLO\nFN/iq1doLNJugXB9+Qzy5yU7xkmBN2gj9yyRpHy1KFV5r7FdyqqAtKaBBKB1zICW\ncUQuMDeLgOLGHdBY6aUJ1DN4dWWmvtw2x3g8ZY8mbK9PTLUlCILx/zwtEk4y0AaU\n8um48mBJgDTrPENRo191vPE2u80aupbkLxw6wlnrAXRpuQOgyx5xsbHmhc5+v6Tm\nkqLD/tBty4sBjqCTsBf1Zh5avXG5XsrLkQbJEVP+yWpLTrwHP3T8phconFvtGCyu\nu0xk3iKTiFqPJOXWWo/tW5Etmoq4xQ1s8l3KQ8OyCSBG9W7YHX/mesWXiDXmqWOj\nIqMcOXa9AgMBAAECggEAJk8kImDZj0ewUzcp7OoNetu0F1FG2Y6x2eBf7oaztxod\nlUUvIt1xoVxPIvxtwcVnf2mLLP8ZKWepRr+1ELHhKDuv63m0JsO/Z2QOUVn0mJg+\n/pBUX1/Wovwmxe/iCWL1nvQIzPYc8JPH7jXyicMKF4kB1Kf92uVPtAg2Jj+OZOjW\nOpZnBPr3a+Z0ev2417uC1uQv995FlqDte3ukO2J/l8WmMTd9bLvYA/5BxcAO2eIO\n/uSOp01cok9MtVR5svTuRO7fPoYo7xzNnv/OFs4SJop4OpSXDx/CO3sp/1ZXrZA5\n7wHe/bF87oStLpeIP1WApWzU+R+x78NRQEcD31hSyQKBgQDu4kZPw3X3uYYBZjlA\nGariKSTW8C7cRmoHIJu9J9wwlKPJiTbmBxmRBVRGs2DzYoSqDyyr2bXdBbeu0beP\nKNeMOd9fZv/ST2V6YtDQ+wbNKWPjKNMB4XdxW0kKs0hEiEc7GzvAG8o7MDZ557K5\nu1x3JmPXArvXVkZnUFIdW71UuQKBgQDKRVfn/ohXHELziqNAQ6EwbNjNFcq7t+11\nANRaDM3aU1m6Q+Pgjg4cDQir7D6ZLanWlWZBPDbYry1W+0wlUizcAinmvdiTMe/6\nw8nb7sld9R9FDCXuTAZsY+xmdACWAyaWD8MxZlowe4SRElsb8a6yxabnxOd8OiH2\n0n6Ii5H4JQKBgQDGoNTv68mCMvvu2+3Mk8sYBDgClHGSbOYqiP2WGbenmWBa6kIA\noHSste6uXubFWOyQbs+pEPx8DA4ASZbEbaRUj6ZhMEaUu7nYOqQAsEmme7z3m/Ff\nxfbIBaxFPm9ftKTty6YyBalw75TMZlCgyN6IoB4yIK7JzY36x7mPecGC2QKBgEEG\ny/z0UkaW+lsrTdJiK5bS11wPTm7M8ZT04E/W3/L5atY77y2I3bZc5lXHDFTYXkpz\nGz2LqyI8JvpYraEt6ugL9gaNL+waWMkoWmPPP4NqQ1RCVItoe4QrF5vUXT/c4XHK\nvXjLK+F8RCETYqD4MK6eZksKfVTtp2+fOjiRjirVAoGASZBm2c4voIgScg/eP3bu\nSw+OlQHEolmh74ePE03YykKWxNnJjWA3eoXOTaw3W0Fw47udNV2wxZu5Je1flnoN\nbH/wxyEJxqqVsoWpXGBS7FcWFEvNfXGJNuJ86WIrVnWQWLslN/zs/aYvR42sM8JR\nv9knrwjeC/9zWuayMnCeCMk=\n-----END PRIVATE KEY-----\n',
+        'client_email': 'firebase-adminsdk-ghh93@uni-pool.iam.gserviceaccount.com',
+        'client_id': '109907487313957185694',
+        'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+        'token_uri': 'https://accounts.google.com/o/oauth2/token',
+        'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+        'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ghh93%40uni-pool.iam.gserviceaccount.com'
     }
-};
+}
