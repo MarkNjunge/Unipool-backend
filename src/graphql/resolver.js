fac6c0a6f7a011b417573dae83035a04ef2b8b33
mecolela <sichangim@gmail.com>
2017-10-16 18:44:23 +0300

    geoLocation type removed

--- src/graphql/resolver.js
+++ src/graphql/resolver.js
@@ -3,8 +3,7 @@ const merge = require('lodash').merge
 const UserResolvers = require('./types/user/resolver')
 const VehicleResolvers = require('./types/vehicle/resolver')
 const RideResolvers = require('./types/ride/resolver')
-const GeolocationResolvers = require('./types/geolocation/resolver')
 
-const resolvers = merge(UserResolvers, VehicleResolvers, RideResolvers, GeolocationResolvers)
+const resolvers = merge(UserResolvers, VehicleResolvers, RideResolvers)
 
 module.exports = resolvers
