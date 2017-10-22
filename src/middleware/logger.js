bb1ecfdbc3b933fa9566ef639372b2f1e5d04721
mecolela <sichangim@gmail.com>
2017-10-22 12:14:04 +0300

    Middleware updates and clean up

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,30 +1,5 @@
 const chalk = require('chalk')
 
-// function logger() {
-//     let args = [].slice.call(arguments);
-//     let type;
-//     let data;
-//     if (args.length === 1) {
-//         data = args[0];
-//     } else if (args.length === 2) {
-//         data = args[0];
-//         type = args[1];
-//     }
-//     if (type) {
-//         if (typeof data === 'object') {
-//             console.dir(data);
-//         } else {
-//             console.log(chalk.red(data));
-//         }
-//     } else {
-//         if (typeof data === 'object') {
-//             console.dir(data);
-//         } else {
-//             console.log(chalk.cyan(data));
-//         }
-//     }
-// }
-
 const logger = {
     log(message) {
         console.log(`Log: ${message}`)
