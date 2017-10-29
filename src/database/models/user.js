7c91a65aa48df18f4c2533459fb1379f5f43a7dd
Mark Njung'e <mark.kamau@outlook.com>
2017-10-29 17:41:15 +0300

    Removed vehicle from user model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,16 +1,14 @@
 'use strict'
 
 const mongoose = require('mongoose')
-const vehicle = require('./vehicle')
 
 const UserSchema = mongoose.Schema({
     _id: String,
-    studentNumber: {type: Number, required: true},
-    email: {type: String, required: true},
-    gender: {type: String, required: true, enum: ['M', 'F']},
-    fullName: {type: String, required: true},
-    phone: {type: Number, required: true},
-    vehicles: [vehicle.schema]
+    studentNumber: { type: Number, required: true },
+    email: { type: String, required: true },
+    gender: { type: String, required: true, enum: ['M', 'F'] },
+    fullName: { type: String, required: true },
+    phone: { type: Number, required: true }
 })
 
 const UserModel = mongoose.model('User', UserSchema)
@@ -18,29 +16,32 @@ const UserModel = mongoose.model('User', UserSchema)
 const User = {
     schema: UserSchema,
     model: UserModel,
-    add: function (details) {
+    add: function(details) {
         const newUser = new this.model(details)
         return newUser.save()
     },
-    find: function (arg) {
+    find: function(arg) {
         if (typeof arg === 'object') {
             return this.model.find(arg)
         }
         return this.model.findById(arg)
     },
-    getVehicles: function (userId) {
+    getVehicles: function(userId) {
         return new Promise((resolve, reject) => {
-            this.model.findById(userId).then(user => {
-                resolve(user.vehicles)
-            }).catch(err => {
-                reject(err)
-            })
+            this.model
+                .findById(userId)
+                .then(user => {
+                    resolve(user.vehicles)
+                })
+                .catch(err => {
+                    reject(err)
+                })
         })
     },
-    delete: function (userId) {
+    delete: function(userId) {
         return this.model.findByIdAndRemove(userId)
     },
-    update: function (details) {
+    update: function(details) {
         if (details._id) {
             let id = details._id
             delete details._id
