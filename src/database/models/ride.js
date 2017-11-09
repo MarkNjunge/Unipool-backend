7639de19839fb385d24956d43434c42380bd4e6b
Mark Njung'e <mark.kamau@outlook.com>
2017-11-09 11:47:51 +0300

    Changed ride operations

--- src/database/models/ride.js
+++ src/database/models/ride.js
@@ -3,26 +3,21 @@ const geoLocation = require('./geolocation')
 
 const PickUp = mongoose.Schema({
     userId: String,
-    time: Number,
-    location: geoLocation.schema
+    time: { type: Number, default: 0 },
+    location: geoLocation.schema,
+    completed: { type: Boolean, default: false }
 })
 
-const passengerLimit = val => val.length <= 4
-
 const RideSchema = mongoose.Schema({
+    rideId: String,
     driverId: String,
     vehicleRegNo: String,
     startLocation: geoLocation.schema,
     endLocation: geoLocation.schema,
     departureTime: Number,
-    passengers: {
-        type: [String],
-        unique: true,
-        validate: [passengerLimit, '{PATH} exceeds passenger limit of 4']
-    },
     pickUps: [PickUp],
     arrivalTime: Number,
-    completed: {type: Boolean, default: false}
+    completed: { type: Boolean, default: false }
 })
 
 const RideModel = mongoose.model('ride', RideSchema)
@@ -30,32 +25,83 @@ const RideModel = mongoose.model('ride', RideSchema)
 const Ride = {
     schema: RideSchema,
     model: RideModel,
-    add: function (details) {
+    add: function(details) {
         const toAdd = new this.model(details)
         return toAdd.save()
     },
-    addPickup: function (details) {
-            let toPick = {
-                userId: details.userId,
-                time: details.time,
-                location: geoLocation.mapLocation(details)
+    addPickup: function(details) {
+        let toPick = {
+            userId: details.userId,
+            location: {
+                latitude: details.latitude,
+                longitude: details.longitude,
+                name: details.locationName
             }
-            return this.model.findOneAndUpdate({_id: details.rideId},
-                {
-                    $push: {pickUp: toPick,
-                            passengers: details.userId}
-                }, {upsert: true})
+        }
+        return this.model.findOneAndUpdate(
+            { rideId: details.rideId },
+            {
+                $push: {
+                    pickUps: toPick
+                }
+            },
+            { upsert: true }
+        )
     },
-    byUser: function (userId) {
-        return this.model.find({user: userId})
+    removePickUp: function(args) {
+        return this.model.findOne({ rideId: args.rideId }, (err, ride) => {
+            if (err) {
+                return err
+            } else {
+                const pickUps = ride.pickUps
+                pickUps.forEach(element => {
+                    if (element.userId == args.userId) {
+                        element.remove()
+                    }
+                }, this)
+                ride.pickUps = pickUps
+                return ride.save((err, updated) => {
+                    if (err) return err
+
+                    return updated
+                })
+            }
+        })
     },
-    get: function (arg) {
+    setPickUpCompleted: function(args) {
+        return this.model.findOne({ rideId: args.rideId }, (err, ride) => {
+            if (err) {
+                return err
+            } else {
+                const pickUps = ride.pickUps
+                pickUps.forEach(element => {
+                    if (element.userId == args.userId) {
+                        element.completed = true
+                        element.time = Date.now()
+                        element.location.name = args.locationName
+                        element.location.latitude = args.latitude
+                        element.location.longitude = args.longitude
+                    }
+                }, this)
+                ride.pickUps = pickUps
+                return ride.save((err, updated) => {
+                    if (err) return err
+
+                    return updated
+                })
+            }
+        })
+    },
+    byUser: function(userId) {
+        return this.model.find({ user: userId })
+    },
+    get: function(arg) {
         if (typeof arg === 'object') {
             return this.model.findById(arg.rideId)
         }
-        return this.model.findOne(arg);
+        return this.model.findOne(arg)
     },
-    update: function (newVersion) {
+    update: function(newVersion) {
         if (newVersion.hasOwnProperty('id')) {
             let id = newVersion.id
             return this.model.findByIdAndUpdate(id, newVersion)
@@ -63,10 +109,20 @@ const Ride = {
             throw new Error('Ride Id expected but none was found')
         }
     },
-    isComplete: function (rideId) {
-        return this.model.findByIdAndUpdate(rideId, {
-            completed: true,
-            arrivalTime: Date.now()
+    setRideCompleted: function(args) {
+        return this.model.findOne({ rideId: args.rideId }, (err, ride) => {
+            ride.endLocation = {
+                latitude: args.latitude,
+                longitude: args.longitude,
+                name: args.locationName
+            }
+            ride.completed = true
+            ride.arrivalTime = Date.now()
+            return ride.save((err, updated) => {
+                if (err) return err
+
+                return updated
+            })
         })
     }
 }
